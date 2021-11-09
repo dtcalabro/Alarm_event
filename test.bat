226e47ab8c22daa5426447ddffe229544cbc5f73
Gaming4Life-YouTube <45155421+Gaming4Life-YouTube@users.noreply.github.com>
2021-11-09 14:02:11 -0500

    Add files via upload

--- test.bat
+++ test.bat
@@ -1,10 +1,13 @@
 @echo off
 
-echo do > "%LOCALAPPDATA%\Temp\volup.vbs"
-echo Set WshShell = CreateObject("WScript.Shell") >> "%LOCALAPPDATA%\Temp\volup.vbs"
-echo WshShell.SendKeys(chr(^&hAF)) >> "%LOCALAPPDATA%\Temp\volup.vbs"
-echo WScript.Sleep 10 >> "%LOCALAPPDATA%\Temp\volup.vbs"
-echo loop >> "%LOCALAPPDATA%\Temp\volup.vbs"
+echo Dim x, y > "%LOCALAPPDATA%\Temp\volup.vbs"
+echo x = Timer() + 18 'add time in seconds >> "%LOCALAPPDATA%\Temp\volup.vbs"
+echo y = 0 >> "%LOCALAPPDATA%\Temp\volup.vbs"
+echo Do >> "%LOCALAPPDATA%\Temp\volup.vbs"
+echo 	Set WshShell = CreateObject("WScript.Shell") >> "%LOCALAPPDATA%\Temp\volup.vbs"
+echo 	WshShell.SendKeys(chr(^&hAF)) >> "%LOCALAPPDATA%\Temp\volup.vbs"
+echo    y = y + 1 >> "%LOCALAPPDATA%\Temp\volup.vbs"
+echo Loop While Timer() ^< x >> "%LOCALAPPDATA%\Temp\volup.vbs"
 
 echo Dim oPlayer > "%LOCALAPPDATA%\Temp\play_alarm.vbs"
 echo Set oPlayer = CreateObject("WMPlayer.OCX") >> "%LOCALAPPDATA%\Temp\play_alarm.vbs"
@@ -15,5 +18,5 @@ echo 	WScript.Sleep 100 >> "%LOCALAPPDATA%\Temp\play_alarm.vbs"
 echo Wend >> "%LOCALAPPDATA%\Temp\play_alarm.vbs"
 echo oPlayer.close >> "%LOCALAPPDATA%\Temp\play_alarm.vbs"
 
-wscript.exe "%LOCALAPPDATA%\Temp\play_alarm.vbs"
-wscript.exe "%LOCALAPPDATA%\Temp\volup.vbs"
+start wscript.exe "%LOCALAPPDATA%\Temp\play_alarm.vbs"
+start wscript.exe "%LOCALAPPDATA%\Temp\volup.vbs"
